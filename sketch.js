c0ec38eddc8f07adcc052930e5b8b488f5f1ab86
강온유 <koy62352@naver.com>
2025-11-25 01:25:04 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,340 +1,219 @@
-let dayColor;
-let nightColor;
-let transitionDuration = 900;
-// 보트 위치
-let boatX = 0; 
-
-
-function setup(){
-  createCanvas(600,400)
-  noStroke();
-  background('#0A1658') // 바다 색상
-  colorMode(RGB);
-
+function setup() {
+  createCanvas(600, 400);
 }
 
-function draw(){
-  background('#0A1658');
-  noStroke()
-  dayColor = color(100, 150, 255); // 낮 색상
-  nightColor = color('#1E2E7F'); // 밤 색상
-  let amount = map(frameCount, 0, transitionDuration, 0.0, 1.0);
-  amount = constrain(amount, 0.0, 1.0);
+function draw() {
+  background('#A0D7FF7F');
+  
 
-  let newBgColor = lerpColor(dayColor, nightColor, amount);
-  
-  // 배경
-  noStroke();
-  fill(newBgColor);
-  rect(0, 0, 600, 300);
-  
-  
-  
-  let time = frameCount * 0.05; // 속도
-  let sizeVariationRange = 1.5;
-  
-  
-  //// 별 그리기
-  fill(255);     
+  
+  stroke('#1A1A1B') // 검정 레인줄
+  strokeWeight(3)
+  line(150,0,150,400)
+  line(310,0,310,400)
+  line(480,0,480,400)
+  
+  stroke('#F44336') // 첫 번째 빨간 레인줄
+  strokeWeight(8)
+  line(150,10,150,50)
+  line(150,100,150,150)
+  line(150,200,150,250)
+  line(150,300,150,350)
+  line(150,390,150,450)
 
-  ellipse(3, 3, 3, 3);
-  ellipse(8, 30, 3, 3);
-  ellipse(10, 20, 2, 2);
-  ellipse(20, 3, 2, 2);
-  ellipse(90, 40, 5, 5);
-  ellipse(20, 40, 5, 5);
-  ellipse(300, 40, 5, 5);
-  ellipse(70, 50, 5, 5);
-  ellipse(20, 25, 5, 5);
-  ellipse(30, 10, 5, 5);
-  ellipse(45, 50, 5, 5);
-  ellipse(80, 10, 4, 4);
-  ellipse(100, 25, 5, 5);
-  ellipse(110, 10, 3, 3);
-  ellipse(120, 15, 4, 4);
-  ellipse(130, 50, 5, 5);
-  ellipse(230, 50, 5, 5);
-  ellipse(430, 50, 5, 5);
-  ellipse(360, 50, 5, 5);
-  ellipse(140, 15, 5, 5);
-  ellipse(150, 5, 5, 5);
-  ellipse(160, 10, 5, 5);
-  ellipse(170, 5, 4, 4);
-  ellipse(180, 15, 5, 5);
-  ellipse(200, 3, 5, 5);
-  ellipse(90, 25, 3, 3);
-  ellipse(40, 25, 3, 3);          
-  ellipse(70, 25, 3, 3);
-  ellipse(60, 15, 3, 3);
-  ellipse(90, 25, 3, 3);
-  ellipse(100, 25, 3, 3);
-  ellipse(150, 25, 3, 3);
-  ellipse(190, 25, 3, 3);
-  ellipse(220, 25, 3, 3);
-  ellipse(250, 25, 3, 3);
-  ellipse(270, 25, 3, 3);
-  ellipse(290, 25, 3, 3);
-  ellipse(210, 10, 3, 3);
-  ellipse(230, 14, 3, 3);
-  ellipse(240, 10, 3, 3);
-  ellipse(280, 8, 3, 3);
-  ellipse(300, 8, 3, 3);
-  ellipse(305, 20, 3, 3);
-  ellipse(310, 8, 3, 3);
-  ellipse(330, 8, 4, 4);
-  ellipse(340, 14, 3, 3);
-  ellipse(355, 8, 3, 3);
-  ellipse(365, 15, 4, 4);
-  ellipse(400, 8, 3, 3);
-  ellipse(450, 8, 3, 3);
-  ellipse(500, 20, 3, 3);
-  ellipse(520, 8, 3, 3);
-  ellipse(535, 8, 4, 4);
-  ellipse(550, 14, 3, 3);
-  ellipse(575, 8, 3, 3);
-  ellipse(600, 15, 4, 4);
-  ellipse(540, 14, 3, 3);
-  ellipse(440, 14, 5, 5);
-  ellipse(480, 8, 5, 5);
-  ellipse(550, 30, 2, 2);
-  ellipse(500, 30, 5, 5);
-  ellipse(470, 25, 4, 4);
-  ellipse(420, 30, 3, 3);
-  
+  
+   // 두번째 레인 빨간색 입힌 것
+  line(310,10,310,50)
+  line(310,100,310,150)
+  line(310,200,310,250)
+  line(310,300,310,350)
+  
+  
+   // 세번째 레인 빨간색 입힌 것
+  line(480,10,480,50)
+  line(480,100,480,150)
+  line(480,200,480,250)
+  line(480,300,480,350)
+  
+  
+  
+  fill('#3D3330')
+  noStroke()
+  ellipse(280,240,110,110) // 뒷 똥머리
+  
+  
+  stroke('#1A1A1B')
+  strokeWeight(1)
+  fill('#FCEA4A')
+  strokeWeight(1)
+  ellipse(350,70,290,300) // 수영모
+  
+  fill('#4ECD53')
+  ellipse(220,100,5,5) // 수영모 무늬 동그라미
+  ellipse(220,100,10,35)
+  ellipse(220,100,20,15)
+  ellipse(245,50,10,35)
+  ellipse(245,50,20,15)
+  ellipse(280,20,10,35)
+  ellipse(280,20,20,15)
+  
+  
+  ellipse(430,20,10,35) // 오른쪽 상단 무늬
+  ellipse(430,20,20,15)
+  
+  ellipse(460,50,10,35)
+  ellipse(460,50,20,15) // 오른쪽 중앙 무늬
+  
+  ellipse(480,100,10,35)
+  ellipse(480,100,20,15) // 오늘쪽 하단 무늬
+  
+  
+  noStroke()
+  fill('#F7F2DA')
+  ellipse(350,400,290,200) // 몸통
+  rect(330,280,50,60) // 목
+  
+  fill('#EAE5CC')
+  ellipse(355,280,50,35) // 목 그림자
+  
+  
+  fill('#FEFEE1')
+  noStroke()
+  ellipse(350,150,230,270) //얼굴
+  ellipse(465,150,40,45) // 귀 오
+  ellipse(235,150,40,45) //귀 왼
+  
+  
+  fill('#2C2827') // 속눈썹
+  stroke('#262526')
+  strokeWeight(2)
+  line(280,80,295,90) // 왼쪽
+  line(270,90,285,100)
+  line(403,90,406,80) // 오른쪽
+  line(425,95,435,82)
+  line(440,103,450,92)
+  
+  strokeWeight(3)
+  noFill() // 눈썹
+  arc(300, 85, 70, 30, radians(180), radians(0) );
+  arc(405, 85, 70, 30, radians(180), radians(0) );
+  
 
-  
-  // 별똥별 1
-  let shootingStarSpeed1 = 5;
-  let tailLength1 = 20;
-  let starTime1 = (frameCount * shootingStarSpeed1) % 800; 
+  noStroke()
+  fill('#060606')
+  ellipse(305,120,80,70) // 눈 밖 아이라이너
+  ellipse(405,120,80,70)
+  
+  
+  fill('#FFFFFF')
+  ellipse(300,125,80,70) // 눈 가장 밖 하얀자
+  ellipse(400,125,80,70)
+  
+  fill('#060606')
+  ellipse(300,125,50,50) // 눈 검정동자
+  ellipse(400,125,50,50)
+  
+  fill('#654337')
+  ellipse(300,125,30,40) // 눈 갈색동자
+  ellipse(400,125,30,40)
+  
 
-  let startX1 = 700 - starTime1 * 1.5;
-  let startY1 = -100 + starTime1 * 0.5;
+  fill('#FFFFFF')
+  ellipse(300,125,10,10) // 눈 제일 안 흰색
+  ellipse(400,125,10,10)
+  
+  
+  
+  fill('#5598B6')
+  stroke('#3577AC')
+  strokeWeight(2)
+  rect(219,125,17,10) // 안경 줄
+  rect(345,125,9,10)
+  rect(465,125,18,10)
+  fill('#5598B672')
+  strokeWeight(5)
+  ellipse(290,125,110,90) // 안경 눈알
+  ellipse(410,125,110,90)
+  
 
-  let angle1 = radians(45);
-  let endX1 = startX1 - tailLength1 * cos(angle1);
-  let endY1 = startY1 + tailLength1 * sin(angle1);
+  
+  stroke(0)
+  strokeWeight(3)
+  noFill();
+  arc(350, 19, 60, 70, 0, HALF_PI); // 앞머리 오
+  arc(340, 18, 60, 70, 0, HALF_PI); // 앞머리 중간
+  arc(330, 17, 60, 70, 0, HALF_PI); // 앞머리 왼
+  
+  
+  
+  
+  
+  arc(335, 160, 30, 50, 0, HALF_PI); // 코
 
-  stroke(255);
-  strokeWeight(2);
-  line(startX1, startY1, endX1, endY1);
+  fill('#7E384F')////////입
+  arc(352, 210, 90, 90, radians(10), radians(190) );
+  stroke('#1A1A1B')
+  strokeWeight(3)
+  line(308,202,397,217)
 
+  stroke('#000000')
+  strokeWeight(3)/////
+  fill('#D63269')
+  ellipse(350,238,60,30)
+  
+  
+  
+  
+  stroke('#747373') //// 귀걸이
+  fill('#E6E4E4')
+  strokeWeight(2)
+  ellipse(470,170,10,10)
+  ellipse(230,170,10,10)
+  
+  noFill()          //// 목걸이
+  stroke('#F9D774')
+  arc(355, 302, 53, 80, radians(0), radians(180) );
 
-  //별똥별 2
-  let shootingStarSpeed2 = 5;
-  let tailLength2 = 20;
+  
+  
+  
+  
+  noStroke()
+  stroke('#000000')////////////////////////////코 아래 
+  noFill()
+  line(335,185,360,180)
+  
+  
+  noStroke()
+  fill('#F86E9D7F')
+  ellipse(270,190,50,30) // 볼터치
+  ellipse(430,190,50,30)
+  fill('#BEB9BB7F')
+  ellipse(227,150,15,22)// 귀 속 음영
+  ellipse(473,150,15,22)
+  
+  
 
-  let starTime2 = ((frameCount + 200) * shootingStarSpeed2) % 800; 
+  fill('#EAE5CC')
+  ellipse(445,395,30,90) // 몸통 그림자
+  
+  
+  fill('#FB578F') // 수영복
+  ellipse(265,333,10,30)
+  ellipse(445,339,10,30)
+  
+  rect(260,330,10,60)//끈
+  rect(440,335,10,60)//끈
+  rect(260,340,190,80)//몸통
+  
+  fill('#F9C1D5')
+  triangle(260,400,300,400,280,340) //수영복 패턴
+  triangle(310,400,350,400,330,340)
+  triangle(360,400,400,400,380,340)
+  triangle(410,400,450,400,430,340)
+  
+  
+  
 
-  let startX2 = 650 - starTime2 * 1.5; 
-  let startY2 = -50 + starTime2 * 0.5;
-
-  let angle2 = radians(45);
-  let endX2 = startX2 - tailLength2 * cos(angle2);
-  let endY2 = startY2 + tailLength2 * sin(angle2);
-
-  stroke(255);
-  strokeWeight(2);
-  line(startX2, startY2, endX2, endY2);
-
-  //별똥별 3
-  let shootingStarSpeed3 = 5;
-  let tailLength3 = 20;
-
-  let starTime3 = ((frameCount + 400) * shootingStarSpeed3) % 800; 
-
-  let startX3 = 1000 - starTime3 * 1.5;
-  let startY3 = -150 + starTime3 * 0.5;
-
-  let angle3 = radians(45);
-  let endX3 = startX3 - tailLength3 * cos(angle3);
-  let endY3 = startY3 + tailLength3 * sin(angle3);
-
-  stroke(255);
-  strokeWeight(2);
-  line(startX3, startY3, endX3, endY3);
-  
-
-  // 보트 위치를 마우스 X 좌표로 설정 (마우스 인터랙션)
-  boatX = mouseX;
-  
-  push();
-  translate(boatX, 0); //위치 이동 
-
-  // 보트
-  noStroke();
-  fill('#FFFFFF')
-  rect(0,250,100,50)
-  fill('#C0C0C0')
-  rect(0,270,150,30)
-  
-  stroke(255)
-  fill('#9B0D0D')
-  strokeWeight(2)
-  line(20,250,20,200)
-  triangle(20,110,20,239,70,239)
-  
-  
-  stroke('#0080FF') //보트유리
-  strokeWeight(2)
-  fill('#CCFFFF')
-  rect(10,255,10,10)
-  rect(30,255,10,10)
-  rect(50,255,10,10)
-  rect(70,255,10,10)
-  
-  pop(); // 보트 애니메이션 끝
-  
-  
-
-//물고기 1
-  let sizeScale1 = map(sin(frameCount * 0.04), -1, 1, 0.9, 1.1);
-
-  push();
-  translate(328, 200); 
-  scale(sizeScale1); 
-  translate(-328, -200);
-  
-  noStroke()
-  fill('#FFC0FF')
-  arc(328, 200, 200, 70, radians(45), radians(270 + 45));
-  triangle(190,150,190,250,230,200)
-  fill(0)
-  ellipse(310,180,10,10)
-  fill('#D171D1')
-  triangle(195,170,195,230,220,200)
-  
-  pop(); // 종료
-  
-// 물고기 2,3
-  let colorAmount1 = (sin(frameCount * 0.08) + 1) / 2;
-  let colorAmount2 = (sin(frameCount * 0.06 + 1.5) + 1) / 2; 
-
-  let startColor1 = color('#FFE0FF'); // 원래 색
-  let endColor1 = color(100, 200, 255); 
-
-  let startColor2 = color('#FFCCE5'); // 원래 색
-  let endColor2 = color(200, 150, 255); 
-
-  let newFishColor2 = lerpColor(startColor1, endColor1, colorAmount1);
-  let newFishColor3 = lerpColor(startColor2, endColor2, colorAmount2);
-
-// 물고기 2
-noStroke();
-fill(newFishColor2)
-  arc(150, 350, 100, 50, radians(45), radians(270 + 45));
- triangle(75,320,75,370,100,345);
-  fill(0)
-  ellipse(140,345,6,6)
-  
-// 물고기 3
-fill(newFishColor3)
-  arc(340, 320, 100, 50, radians(45), radians(270 + 45));
-  triangle(250,295,250,345,300,320);
-  fill(0)
-  ellipse(330,310,6,6)
-  
-
-  
-  fill('#663300')
-  rect(550,100,60,300)
-  fill('#804000')
-  rect(570,100,60,300)
-  fill('#994C00')
-  rect(590,100,60,300)
-  
-  fill('#3C5E6E')
-  ellipse(590,400,300,300)
-  
-  
-  fill('#80C080')
-  ellipse(500,100,100,50)
-  ellipse(520,70,100,50)
-  ellipse(550,120,100,50)
-  ellipse(590,80,100,50)
-  ellipse(600,50,100,50)
-  ellipse(590,120,100,50)
-  
-  stroke(0)
-  strokeWeight(1)
-  fill('#C2E2E2')
-  ellipse(480,270,20,30)
-  ellipse(470,280,20,30)
-  fill('#B4B4C7')
-  ellipse(510,270,50,30)
-  ellipse(500,280,50,30)
-  
-  fill('#FFFFE5');
-  circle(500,240,10)
-  fill(0)
-  triangle(500,240,515,230,515,250)
-  
-  fill('#80C0FF')
-  rect(530,220,10,20);
-  ellipse(530,250,40,60)
-    fill('#FFFFE5');
-  circle(530,200,50);
-
-  fill(0);
-  ellipse(520,200,5);
-  triangle(530,240,515,260,540,260);
-  fill('#FFFFE5');
-  circle(515,260,10)
-
-  
-  noStroke()
-  fill('#FFFFA0')
-  ellipse(530,185,70,10);
-  ellipse('#FFFFC0');
-  rect(504,165,55,20);
-
-  
-  fill('#FF3333')
-  ellipse(500,100,30,20)
-  ellipse(580,60,40,30)
-  ellipse(550,130,30,20)
-  
-  stroke(0);
-  strokeWeight(1);
-  line(500, 85, 500, 95);
-  line(580, 40, 580, 53);
-  line(550, 115, 550, 130);
-  
-
-  line(540,290,510,320);
-  line(540,290,570,320);
-  line(570,320,600,290);
-  
-  line(450,380,480,350);
-  line(480,350,510,380);
-  line(510,380,540,350);
-  line(540,350,570,380);
-  line(570,380,600,350);
-  
-  stroke(225)
-  strokeWeight(3)
-  line(510,260,480,230);
-  line(480,230,330,200);
-
-  
-  stroke('#FF8080')
-    fill('#FFC0C0')
-  ellipse(535,210,20,10);
-  
-  stroke(0)
-  strokeWeight(3)
-  line(290,180,320,160)
-  
-  
-
-  stroke(0)
-  strokeWeight(1)
-    noFill();
-  arc(512, 208, 20, 20, 0, HALF_PI);
-}
-  
-function keyPressed() {
-  if (key === 's') {
-    saveGif('mySketch', 10);
-  }
+  
 }
